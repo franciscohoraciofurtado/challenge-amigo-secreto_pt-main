ebe265c59736223669bf06518c7b94f4c46dbd87
franciscohoracio <franciscohoraciofurtado@gmail.com>
2025-01-28 20:40:32 -0300

    codigo_final

---
+++ README.md
@@ -0,0 +1,19 @@
+# Amigo Secreto
+
+O projeto tem como finalidade receber uma lista de nomes e sortear um nome da lista para amigo secreto.
+
+## Funcionalidades
+
+- Função adicionar amigo onde é repassado um nome por vez e adicionado em uma lista
+- É exibido na tela a lista de nomes que está sendo adicionado.
+- Função sortear amigo onde ao clicar no botão um nome da lista é sorteado e exibido na tela
+
+## Tecnologias Utilizadas
+
+- JavacScript, CSS, HTML
+
+## Como Executar o Projeto
+
+1. Clone este repositório:
+   ```bash
+   git clone https://github.com/franciscohoraciofurtado/challenge-amigo-secreto_pt-main
--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let numeroDeCliqueEmAdd = 0;
+let listaDeAmigos = [];
+let listaAmigos = [];
+
+function adicionarAmigo(){
+    let valor = document.getElementById("amigo").value;
+    if (valor == ''){
+        alert("Digite um Nome")
+    }else{
+        listaAmigos.push(document.getElementById("amigo").value);
+        console.log(listaAmigos);
+        document.getElementById("amigo").value = "";
+        atualizaLista();
+
+    }
+}
+
+function sortearAmigo(){
+    listaDeAmigos.innerHTML = "";
+    let amigoSorteado = parseInt(Math.random() * numeroDeCliqueEmAdd);
+    console.log(listaAmigos[amigoSorteado]);
+    nomeSorteado = document.querySelector('ul');
+    nomeSorteado.innerHTML = "O Seu amigo Secreto é "+ listaAmigos[amigoSorteado];
+    console.log();
+}
+
+function atualizaLista(){
+    listaDeAmigos = document.querySelector('ul');
+    listaDeAmigos.innerHTML = "";
+    for(let i = 0; i < listaAmigos.length; i++){
+       let li = document.createElement('li'); 
+       li.textContent = listaAmigos[i]; 
+       listaDeAmigos.appendChild(li); 
+   }
+}
